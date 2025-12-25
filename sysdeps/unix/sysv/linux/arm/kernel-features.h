5bcd6435b17d4db83e47b837ce6e6fea1da81d3b
Bingchen Gong <6704443+Wenri@users.noreply.github.com>
2025-12-25 18:27:25 +0100

    termux: set kernel feature flags for Android
    Disable __ASSUME_CLONE3 and other features not available on Android.
    
    Based on Termux glibc-packages kernel-features.h.patch
    
    (cherry picked from commit c547fc15495f7e0854fff440a0f65ab8d8a358f0)

--- sysdeps/unix/sysv/linux/arm/kernel-features.h
+++ sysdeps/unix/sysv/linux/arm/kernel-features.h
@@ -38,9 +38,6 @@
    non-standard name.  */
 #define __NR_fadvise64_64 __NR_arm_fadvise64_64
 
-#define __ASSUME_RECV_SYSCALL   1
-#define __ASSUME_SEND_SYSCALL	1
-
 /* Support for the mlock2 and copy_file_range syscalls was added to
    the compat syscall table for 64-bit kernels in 4.7, although
    present in 32-bit kernels from 4.4 and 4.5 respectively.  */
